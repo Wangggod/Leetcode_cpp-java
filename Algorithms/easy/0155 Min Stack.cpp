9f9bb3e6e35c109ca1fa9ed530239f4057b102bc
Wangggod <34480521+Wangggod@users.noreply.github.com>
2020-03-06 21:10:40 +0800

    2020.3.6

---
+++ Algorithms/easy/0155 Min Stack.cpp
@@ -0,0 +1,41 @@
+// Source: https://leetcode.com/problems/min-stack/
+// Author: Wangggod
+// Date: 2020.3.6
+
+class MinStack {
+    stack<pair<int, int>> st;
+public:
+    /** initialize your data structure here. */
+
+    void push(int x) {
+        int min;
+        if (st.empty()) {
+            min = x;
+        }
+        else {
+            min = std::min(st.top().second,x);
+        }
+        st.push({x, min});
+    }
+
+    void pop() {
+        st.pop();
+    }
+
+    int top() {
+        return st.top().first;
+    }
+
+    int getMin() {
+        return st.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
