b2631331793056a9a05af5c880e96cccf8de1a2f
Wangggod <34480521+Wangggod@users.noreply.github.com>
2020-03-09 23:23:37 +0800

    2020.3.9

---
+++ Algorithms/easy/0206 Reverse Linked List.cpp
@@ -0,0 +1,27 @@
+// Source: https://leetcode.com/problems/reverse-linked-list/
+// Author: Wangggod
+// Date: 2020.3.9
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = NULL;
+        ListNode *cur = head;
+        ListNode *next = NULL;
+        while (cur != NULL) {
+            next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+};
