e8f0aa6c7fed2d8948c587555efeab0bd6419171
Wangggod <34480521+Wangggod@users.noreply.github.com>
2020-03-14 17:48:05 +0800

    2020.3.14

---
+++ Algorithms/easy/0349 Intersection of Two Arrays.cpp
@@ -0,0 +1,17 @@
+// Source: https://leetcode.com/problems/intersection-of-two-arrays/
+// Author: Wangggod
+// Date: 2020.3.14
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> m(nums1.begin(),nums1.end());
+        vector<int> res;
+        for (auto a : nums2) {
+            if (m.count(a)) {
+                res.push_back(a);
+                m.erase(a);
+            }
+        }
+        return res;
+    }
+};
